db662a5bb5c3d8afa4a38eca4706c2ba54ea029c
Pavel Janousek <pjanouse@redhat.com>
2016-11-01 08:13:43 +0100

    [JENKINS-38778] Fix leaked node without releasing (#24)
    * [JENKINS-38778] Fix leaked node without releasing

--- src/main/java/com/redhat/foreman/ForemanComputerListener.java
+++ src/main/java/com/redhat/foreman/ForemanComputerListener.java
@@ -1,6 +1,8 @@
 package com.redhat.foreman;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import hudson.Extension;
 import hudson.model.Computer;
@@ -13,10 +15,19 @@ import hudson.slaves.ComputerListener;
 @Extension
 public class ForemanComputerListener extends ComputerListener {
 
+    private static final Logger LOGGER = Logger.getLogger(ForemanComputerListener.class.getName());
+
     @Override
     public void onLaunchFailure(Computer c, TaskListener taskListener) throws IOException, InterruptedException {
-        super.onLaunchFailure(c, taskListener);
-        ForemanComputer.terminateForemanComputer(c);
+        try {
+            super.onLaunchFailure(c, taskListener);
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Uncaught unexpected exception occurred while calling super.onLaunchFailed(): ", e);
+        }
+        if (c instanceof ForemanComputer) {
+            LOGGER.info("Launch of the Computer '" + c.getDisplayName() + "' failed, releasing...");
+            ((ForemanComputer) c).eagerlyReturnNodeLater();
+        }
     }
 
 }
