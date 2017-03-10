61717a16122ba446660182e4d96c06c7b3c69a5d
Oliver Gondža <ogondza@gmail.com>
2017-03-10 14:18:58 +0100

    Introduce HostInfo

--- src/test/java/com/redhat/foreman/ForemanSharedNodeCloudTest.java
+++ src/test/java/com/redhat/foreman/ForemanSharedNodeCloudTest.java
@@ -28,20 +28,26 @@ import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static com.redhat.foreman.TestUtils.readFile;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.charset.Charset;
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 
 import javax.servlet.ServletException;
 
-import hudson.model.AdministrativeMonitor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import hudson.model.FreeStyleBuild;
 import hudson.util.OneShotEvent;
 import org.jenkinsci.plugins.resourcedisposer.AsyncResourceDisposer;
@@ -51,7 +57,6 @@ import org.junit.Test;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.redhat.foreman.ForemanSharedNodeCloud.DescriptorImpl;
-import com.redhat.foreman.launcher.ForemanDummyComputerLauncherFactory;
 
 import hudson.model.Computer;
 import hudson.model.FreeStyleProject;
@@ -68,9 +73,12 @@ public class ForemanSharedNodeCloudTest {
     private static final int HTTPOK = 200;
     private static final int HTTPERROR = 500;
 
-    private static final String URL = "http://localhost:32789/api";
+    private static final int SERVICE_PORT = new Random().nextInt(100) + 32000;
+
+    private static final String URL = "http://localhost:" + SERVICE_PORT + "/api";
     private static final String USER = "admin";
     private static final String PASSWORD = "changeme";
+    private static final String SUT_HOSTNAME = "localhost.localdomain";
 
     /**
      * Rule for Jenkins.
@@ -82,112 +90,44 @@ public class ForemanSharedNodeCloudTest {
      * Rule for wiremock.
      */
     @Rule
-    public final WireMockRule wireMockRule = new WireMockRule(32789);
+    public final WireMockRule wireMockRule = new WireMockRule(SERVICE_PORT);
 
     /**
      * Prepare wiremocks.
-     *
-     * @throws IOException if occurs.
-     * @throws URISyntaxException if occurs.
      */
+    // TODO: Avoid shared fixture
     private void setupWireMock() throws IOException, URISyntaxException {
-        String body1  = TestUtils.readFile("body1.txt", Charset.forName("UTF-8"));
-        String body2  = TestUtils.readFile("body2.txt", Charset.forName("UTF-8"));
-        String body3  = TestUtils.readFile("body3.txt", Charset.forName("UTF-8"));
-        String body4  = TestUtils.readFile("body4.txt", Charset.forName("UTF-8"));
-        String body5  = TestUtils.readFile("body5.txt", Charset.forName("UTF-8"));
-        String body6  = TestUtils.readFile("body6.txt", Charset.forName("UTF-8"));
-        String body7  = TestUtils.readFile("body7.txt", Charset.forName("UTF-8"));
-        String body8  = TestUtils.readFile("body8.txt", Charset.forName("UTF-8"));
-        String body9  = TestUtils.readFile("body9.txt", Charset.forName("UTF-8"));
-        String body10 = TestUtils.readFile("body10.txt", Charset.forName("UTF-8"));
-        String body11 = TestUtils.readFile("body11.txt", Charset.forName("UTF-8"));
-
         stubFor(get(urlEqualTo("/api/v2/hosts?search=params.JENKINS_LABEL%3Dlabel1"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body1)));
+                .willReturn(ok("body1.txt")));
 
         stubFor(get(urlEqualTo("/api/v2/hosts?search=params.JENKINS_LABEL%3Dlabel1+and+params.RESERVED%3D"
                 + "false+and+has+params.JENKINS_SLAVE_REMOTEFS_ROOT"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body2)));
-
-        stubFor(get(urlMatching("/api/hosts_reserve.+"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body3)));
+                .willReturn(ok("body2.txt")));
 
         stubFor(get(urlEqualTo("/api/v2/hosts?search=params.JENKINS_LABEL%3Dlocalhost.localdomain"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body4)));
-
-        stubFor(get(urlEqualTo("/api/hosts_release?query=name+~+localhost.localdomain"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body5)));
+                .willReturn(ok("body4.txt")));
 
         stubFor(get(urlEqualTo("/api/v2/hosts"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body1)));
+                .willReturn(ok("body1.txt")));
 
         stubFor(get(urlEqualTo("/api/v2/hosts?search=params.JENKINS_LABEL%3Dlabel1+and+params.RESERVED%3Dfalse"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body2)));
+                .willReturn(ok("body2.txt")));
 
         stubFor(get(urlEqualTo("/api/v2/hosts/localhost.localdomain/parameters/JENKINS_SLAVE_REMOTEFS_ROOT"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body6)));
-
-        stubFor(get(urlEqualTo("/api/v2/hosts/localhost.localdomain"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body7)));
-
-        stubFor(get(urlEqualTo("/api/v2/status"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body8)));
+                .willReturn(ok("body6.txt")));
 
         stubFor(get(urlEqualTo("/api/v2/hosts?search=has+params.JENKINS_LABEL+"
                 + "and+has+params.RESERVED+and+has+params.JENKINS_SLAVE_REMOTEFS_ROOT"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body9)));
+                .willReturn(ok("body9.txt")));
 
         stubFor(get(urlEqualTo("/api/v2/hosts/localhost.localdomain/parameters/JENKINS_LABEL"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body10)));
+                .willReturn(ok("body10.txt")));
 
         stubFor(get(urlEqualTo("/api/v2/hosts/localhost.localdomain/parameters/RESERVED"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body11)));
+                .willReturn(ok("body11.txt")));
 
         stubFor(get(urlEqualTo("/api/v2/hosts?search=params.RESERVED%3Dfalse"))
-                .willReturn(aResponse()
-                        .withStatus(HTTPOK)
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(body2)));
+                .willReturn(ok("body2.txt")));
 
     }
 
@@ -214,10 +154,13 @@ public class ForemanSharedNodeCloudTest {
      * @throws URISyntaxException if occurs.
      */
     @Test
-    public void doTestConnection() throws ServletException, IOException, URISyntaxException {
-        setupWireMock();
+    public void doTestConnection() throws Exception {
+        stubServiceStatus();
         DescriptorImpl descr = new ForemanSharedNodeCloud.DescriptorImpl();
-        descr.doTestConnection(URL, USER, Secret.fromString(PASSWORD));
+        assertThat(
+                descr.doTestConnection(URL, USER, Secret.fromString(PASSWORD)).getMessage(),
+                containsString("1.5.3")
+        );
     }
 
     /**
@@ -230,7 +173,6 @@ public class ForemanSharedNodeCloudTest {
         Computer[] computers = j.jenkins.getComputers();
         int initialComputerSet = computers.length;
 
-        setupWireMock();
         j.addForemanCloud("mycloud", URL, USER, PASSWORD);
 
         FreeStyleProject job = j.createFreeStyleProject();
@@ -307,11 +249,140 @@ public class ForemanSharedNodeCloudTest {
                 .willReturn(aResponse()
                         .withStatus(HTTPOK)
                         .withHeader("Content-Type", "text/json")
-                        .withBody(TestUtils.readFile("body11.txt",
-                                Charset.forName("UTF-8")).replace("false",
-                                "Reserved for "+j.getInstance().getRootUrl()))));
+                        .withBody(readFile("body11.txt").replace("false", "Reserved for "+j.getInstance().getRootUrl()))));
 
         // Wait for cleanup procedure
         j.waitForEmptyAsyncResourceDisposer();
+    }
+
+    @Test
+    public void getHostData() throws Exception {
+        ForemanSharedNodeCloud orig = j.addForemanCloud("mycloud", URL, USER, PASSWORD);
+        ForemanAPI api = orig.getForemanAPI();
+
+        stubHostInfoIdle();
+
+        HostInfo hi = api.getHostInfo("localhost.localdomain");
+        assertEquals("localhost.localdomain", hi.getName());
+        assertEquals("label1", hi.getLabels());
+        assertEquals("/tmp/remoteFSRoot", hi.getRemoteFs());
+        assertEquals(null, hi.getReservedFor());
+        assertFalse(hi.isReserved());
+
+        String reserveReason = stubHostInfoReservedForMe();
+
+        hi = api.getHostInfo("localhost.localdomain");
+        assertEquals("localhost.localdomain", hi.getName());
+        assertEquals("label1", hi.getLabels());
+        assertEquals("/tmp/remoteFSRoot", hi.getRemoteFs());
+        assertEquals(reserveReason, hi.getReservedFor());
+        assertTrue(hi.isReserved());
+
+        assertEquals(null, api.getHostInfo("no_such_host"));
+
+        stubFor(get(urlEqualTo("/api/v2/hosts/broken.host")).willReturn(aResponse().withStatus(500).withBody("Boom!")));
+        try {
+            api.getHostInfo("broken.host");
+            fail();
+        } catch (ForemanAPI.CommunicationError ex) {
+            assertThat(ex.getMessage(), containsString("500"));
+        }
+    }
+
+    @Test
+    public void getVersion() throws Exception {
+        ForemanSharedNodeCloud orig = j.addForemanCloud("mycloud", URL, USER, PASSWORD);
+        ForemanAPI api = orig.getForemanAPI();
+
+        try {
+            api.getVersion();
+            fail();
+        } catch (ForemanAPI.CommunicationError ex) {
+            // Expected
+        }
+
+        stubServiceStatus();
+
+        assertEquals("1.5.3", api.getVersion());
+    }
+
+    @Test
+    public void reserveHost() throws Exception {
+        HostInfo freeHost = new ObjectMapper().readerFor(HostInfo.class).readValue(readFile("host-info-idle.txt"));
+
+        ForemanSharedNodeCloud orig = j.addForemanCloud("mycloud", URL, USER, PASSWORD);
+        ForemanAPI api = orig.getForemanAPI();
+
+        assertEquals(null, api.reserveHost(freeHost));
+
+        stubFor(get(urlEqualTo("/api/v2/hosts/localhost.localdomain"))
+                .inScenario("reserveHost")
+                .willReturn(ok("host-info-idle.txt")));
+        stubFor(get(urlMatching("/api/hosts_reserve.+"))
+                .inScenario("reserveHost")
+                .willSetStateTo("reserved")
+                .willReturn(ok("host-reserved-success.txt")));
+        stubFor(get(urlEqualTo("/api/v2/hosts/localhost.localdomain"))
+                .inScenario("reserveHost")
+                .whenScenarioStateIs("reserved")
+                .willReturn(ok("host-info-reserved-for-me.txt", ForemanAPI.getReserveReason())));
+
+        HostInfo reservedHost = api.reserveHost(freeHost);
+        assertEquals(ForemanAPI.getReserveReason(), reservedHost.getReservedFor());
+        assertEquals("localhost.localdomain", reservedHost.getName());
+    }
+
+    @Test
+    public void releaseHost() throws Exception {
+        String reserveReason = ForemanAPI.getReserveReason();
+
+        ForemanSharedNodeCloud orig = j.addForemanCloud("mycloud", URL, USER, PASSWORD);
+        ForemanAPI api = orig.getForemanAPI();
+
+        stubFor(get(urlEqualTo("/api/v2/hosts/localhost.localdomain"))
+                .inScenario("releaseHost")
+                .willReturn(ok("host-info-reserved-for-me.txt", reserveReason)));
+        stubFor(get(urlEqualTo("/api/hosts_release?query=name+~+localhost.localdomain"))
+                .inScenario("releaseHost")
+                .willSetStateTo("released")
+                .willReturn(ok("host-release-success.txt")));
+        stubFor(get(urlEqualTo("/api/v2/hosts/localhost.localdomain"))
+                .inScenario("releaseHost")
+                .whenScenarioStateIs("released")
+                .willReturn(ok("host-info-idle.txt")));
+
+        assertEquals(reserveReason, api.getHostInfo(SUT_HOSTNAME).getReservedFor());
+        api.release(SUT_HOSTNAME);
+        assertEquals(null, api.getHostInfo(SUT_HOSTNAME).getReservedFor());
+
+        // Call should be idempotent
+        api.release(SUT_HOSTNAME);
+        assertEquals(null, api.getHostInfo(SUT_HOSTNAME).getReservedFor());
+
+        // And deal with host removal
+        api.release("so_such_host");
+        assertEquals(null, api.getHostInfo(SUT_HOSTNAME).getReservedFor());
+    }
+
+    private void stubServiceStatus() {
+        stubFor(get(urlEqualTo("/api/v2/status")).willReturn(ok("service-status.txt")));
+    }
+
+    private String stubHostInfoReservedForMe() throws IOException, URISyntaxException {
+        String reserveReason = ForemanAPI.getReserveReason();
+        stubFor(get(urlEqualTo("/api/v2/hosts/localhost.localdomain")).willReturn(ok("host-info-reserved-for-me.txt", reserveReason)));
+        return reserveReason;
+    }
+
+    private void stubHostInfoIdle() throws IOException, URISyntaxException {
+        stubFor(get(urlEqualTo("/api/v2/hosts/localhost.localdomain")).willReturn(ok("host-info-idle.txt")));
+    }
+
+    private ResponseDefinitionBuilder ok(String path, String... args) {
+        String body = String.format(readFile(path), args);
+        return aResponse()
+                .withStatus(HTTPOK)
+                .withHeader("Content-Type", "text/json")
+                .withBody(body);
     }
 }
