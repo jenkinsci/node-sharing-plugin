87a8aee53092f70549451bf25af0abcea9c75678
Oliver Gondža <ogondza@gmail.com>
2017-10-31 16:07:01 +0100

    Reuse test helpers

---
+++ backend-plugin/src/test/java/com/redhat/jenkins/nodesharingbackend/NodeSharingJenkinsRule.java
@@ -0,0 +1,132 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) Red Hat, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.redhat.jenkins.nodesharingbackend;
+
+import hudson.EnvVars;
+import hudson.model.Executor;
+import hudson.model.Label;
+import hudson.model.Node;
+import hudson.model.Queue;
+import hudson.util.OneShotEvent;
+import hudson.util.StreamTaskListener;
+import jenkins.model.queue.AsynchronousExecution;
+import org.apache.commons.io.FileUtils;
+import org.jenkinsci.plugins.gitclient.Git;
+import org.jenkinsci.plugins.gitclient.GitClient;
+import org.junit.rules.TemporaryFolder;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import static com.redhat.jenkins.nodesharingbackend.Pool.CONFIG_REPO_PROPERTY_NAME;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class NodeSharingJenkinsRule extends JenkinsRule {
+
+    public final ExecutorJenkins DUMMY_OWNER = new ExecutorJenkins("https://jenkins42.acme.com", "jenkins42");
+
+    // Hook it in
+    private TemporaryFolder tmp = new TemporaryFolder();
+
+    @Override
+    public void before() throws Throwable {
+        super.before();
+        tmp.create();
+    }
+
+    @Override
+    public void after() throws Exception {
+        tmp.delete();
+        super.after();
+    }
+
+    protected @Nonnull SharedNode getNode(String name) {
+        Node node = jenkins.getNode(name);
+        assertNotNull("No such node " + name, node);
+        return (SharedNode) node;
+    }
+
+    protected GitClient injectDummyConfigRepo() throws Exception {
+        File orig = new File(getClass().getResource("dummy_config_repo").toURI());
+        assertTrue(orig.isDirectory());
+        File repo = tmp.newFolder();
+        FileUtils.copyDirectory(orig, repo);
+
+        StreamTaskListener listener = new StreamTaskListener(System.err, Charset.defaultCharset());
+        GitClient git = Git.with(listener, new EnvVars()).in(repo).using("git").getClient();
+        git.init();
+        git.add("*");
+        git.commit("Init");
+
+        System.setProperty(CONFIG_REPO_PROPERTY_NAME, repo.getAbsolutePath());
+        Pool.Updater.getInstance().doRun();
+
+        return git;
+    }
+
+    protected static class BlockingTask extends MockTask {
+        final OneShotEvent running = new OneShotEvent();
+        final OneShotEvent done = new OneShotEvent();
+
+        public BlockingTask(Label label) {
+            super(DUMMY_OWNER, label);
+        }
+
+        @Override public void perform() {
+            running.signal();
+            try {
+                done.block();
+            } catch (InterruptedException e) {
+                // Proceed
+            }
+        }
+    }
+
+    protected static class MockTask extends ReservationTask {
+        final SharedComputer actuallyRunOn[] = new SharedComputer[1];
+        public MockTask(@Nonnull ExecutorJenkins owner, @Nonnull Label label) {
+            super(owner, label);
+        }
+
+        @Override
+        public @CheckForNull Queue.Executable createExecutable() throws IOException {
+            return new ReservationExecutable(this) {
+                @Override
+                public void run() throws AsynchronousExecution {
+                    actuallyRunOn[0] = (SharedComputer) Executor.currentExecutor().getOwner();
+                    perform();
+                }
+            };
+        }
+
+        public void perform() {
+            // NOOOP until overriden
+        }
+    }
+}
--- backend-plugin/src/test/java/com/redhat/jenkins/nodesharingbackend/PoolTest.java
+++ backend-plugin/src/test/java/com/redhat/jenkins/nodesharingbackend/PoolTest.java
@@ -1,32 +1,44 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) Red Hat, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.redhat.jenkins.nodesharingbackend;
 
-import hudson.EnvVars;
+import com.redhat.jenkins.nodesharingbackend.NodeSharingJenkinsRule.BlockingTask;
+import com.redhat.jenkins.nodesharingbackend.NodeSharingJenkinsRule.MockTask;
 import hudson.FilePath;
 import hudson.model.Computer;
-import hudson.model.Executor;
 import hudson.model.Label;
 import hudson.model.Node;
 import hudson.model.Queue;
 import hudson.model.queue.ScheduleResult;
 import hudson.slaves.DumbSlave;
-import hudson.util.OneShotEvent;
-import hudson.util.StreamTaskListener;
-import jenkins.model.queue.AsynchronousExecution;
 import jenkins.util.Timer;
-import org.apache.commons.io.FileUtils;
 import org.hamcrest.Matchers;
-import org.jenkinsci.plugins.gitclient.Git;
 import org.jenkinsci.plugins.gitclient.GitClient;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-import org.jvnet.hudson.test.JenkinsRule;
 
-import javax.annotation.CheckForNull;
-import javax.annotation.Nonnull;
-import java.io.File;
-import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
@@ -35,7 +47,6 @@ import java.util.Random;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
-import static com.redhat.jenkins.nodesharingbackend.Pool.CONFIG_REPO_PROPERTY_NAME;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.arrayWithSize;
 import static org.hamcrest.Matchers.containsInAnyOrder;
@@ -49,13 +60,8 @@ import static org.junit.Assert.assertTrue;
 
 public class PoolTest {
 
-    private static final ExecutorJenkins DUMMY_OWNER = new ExecutorJenkins("https://jenkins42.acme.com", "jenkins42");
-
     @Rule
-    public JenkinsRule j = new JenkinsRule();
-
-    @Rule
-    public TemporaryFolder tmp = new TemporaryFolder();
+    public NodeSharingJenkinsRule j = new NodeSharingJenkinsRule();
 
     @Test
     public void inactiveWithNoProperty() throws Exception {
@@ -68,7 +74,7 @@ public class PoolTest {
 
     @Test
     public void readConfigFromRepo() throws Exception {
-        injectDummyConfigRepo();
+        j.injectDummyConfigRepo();
         Pool pool = Pool.getInstance();
         Map<String, String> config = pool.getConfig().getConfig();
         assertEquals("https://dummy.test", config.get("orchestrator.url"));
@@ -84,8 +90,8 @@ public class PoolTest {
 
     @Test
     public void populateComputers() throws Exception {
-        GitClient git = injectDummyConfigRepo();
-        Node win1 = getNode("win1.acme.com");
+        GitClient git = j.injectDummyConfigRepo();
+        Node win1 = j.getNode("win1.acme.com");
         assertEquals("windows w2k12", win1.getLabelString());
         assertTrue(win1.toComputer().isOnline());
 
@@ -100,21 +106,21 @@ public class PoolTest {
             Pool.Updater.getInstance().doRun();
 
             assertThat(j.jenkins.getComputers(), arrayWithSize(4));
-            assertSame(win1, getNode("win1.acme.com"));
-            assertSame(win1.toComputer(), getNode("win1.acme.com").toComputer());
+            assertSame(win1, j.getNode("win1.acme.com"));
+            assertSame(win1.toComputer(), j.getNode("win1.acme.com").toComputer());
         }
     }
 
     @Test
     public void updateComputers() throws Exception {
         DumbSlave doNotTouchMe = j.createOnlineSlave(); // There is no reason for using some other slave kinds on orchestrator but ...
-        GitClient git = injectDummyConfigRepo();
+        GitClient git = j.injectDummyConfigRepo();
 
-        assertEquals("windows w2k16", getNode("win2.acme.com").getLabelString());
-        assertEquals("solaris11 sparc", getNode("solaris1.acme.com").getLabelString());
+        assertEquals("windows w2k16", j.getNode("win2.acme.com").getLabelString());
+        assertEquals("solaris11 sparc", j.getNode("solaris1.acme.com").getLabelString());
         assertNull(j.jenkins.getNode("windows.acme.com"));
 
-        Node nodeW1 = getNode("win1.acme.com");
+        Node nodeW1 = j.getNode("win1.acme.com");
         Computer computerW1 = nodeW1.toComputer();
 
         // Update
@@ -127,33 +133,33 @@ public class PoolTest {
         git.commit("Update");
         Pool.Updater.getInstance().doRun();
 
-        assertEquals("windows w2k16", getNode("windows.acme.com").getLabelString());
-        assertEquals("solaris12 sparc", getNode("solaris1.acme.com").getLabelString());
+        assertEquals("windows w2k16", j.getNode("windows.acme.com").getLabelString());
+        assertEquals("solaris12 sparc", j.getNode("solaris1.acme.com").getLabelString());
         assertNull(j.jenkins.getNode("win2.acme.com"));
         assertNull(j.jenkins.getComputer("win2.acme.com"));
-        assertSame(nodeW1, getNode("win1.acme.com"));
-        assertSame(computerW1, getNode("win1.acme.com").toComputer());
+        assertSame(nodeW1, j.getNode("win1.acme.com"));
+        assertSame(computerW1, j.getNode("win1.acme.com").toComputer());
 
         assertNotNull(j.jenkins.getNode(doNotTouchMe.getNodeName()));
     }
 
     @Test
     public void workloadMapping() throws Exception {
-        injectDummyConfigRepo();
+        j.injectDummyConfigRepo();
 
-        MockTask task = new MockTask(DUMMY_OWNER, Label.get("solaris11"));
+        MockTask task = new MockTask(j.DUMMY_OWNER, Label.get("solaris11"));
         Queue.Item item = task.schedule();
         assertEquals("jenkins42", item.task.getFullDisplayName());
         item.getFuture().get();
-        assertEquals(getNode("solaris1.acme.com").toComputer(), task.actuallyRunOn[0]);
+        assertEquals(j.getNode("solaris1.acme.com").toComputer(), task.actuallyRunOn[0]);
 
 
-        task = new MockTask(DUMMY_OWNER, Label.get("windows"));
+        task = new MockTask(j.DUMMY_OWNER, Label.get("windows"));
         task.schedule().getFuture().get();
         assertThat(task.actuallyRunOn[0].getName(), startsWith("win"));
 
         // Never schedule labels we do not serve - including empty one
-        task = new MockTask(DUMMY_OWNER, Label.get(""));
+        task = new MockTask(j.DUMMY_OWNER, Label.get(""));
         ScheduleResult scheduleResult = j.jenkins.getQueue().schedule2(task, 0);
         assertTrue(scheduleResult.isAccepted());
         assertFalse(scheduleResult.isRefused());
@@ -166,22 +172,22 @@ public class PoolTest {
     @Test
     public void waitUntilComputerGetsIdleBeforeDeleting() throws Exception {
         final String DELETED_NODE = "solaris1.acme.com";
-        GitClient git = injectDummyConfigRepo();
+        GitClient git = j.injectDummyConfigRepo();
 
         BlockingTask task = new BlockingTask(Label.get("solaris11"));
         task.schedule();
         task.running.block();
-        assertFalse("Computer occupied", getNode(DELETED_NODE).toComputer().isIdle());
+        assertFalse("Computer occupied", j.getNode(DELETED_NODE).toComputer().isIdle());
 
         assertTrue(git.getWorkTree().child("nodes").child(DELETED_NODE + ".xml").delete());
         git.add("*");
         git.commit("Remove running node from config repo");
         Pool.Updater.getInstance().doRun();
 
-        assertFalse("Node still exists and occupied", getNode(DELETED_NODE).toComputer().isIdle());
+        assertFalse("Node still exists and occupied", j.getNode(DELETED_NODE).toComputer().isIdle());
         Thread.sleep(1000); // It is not an accident
         Pool.Updater.getInstance().doRun(); // Trigger the check
-        assertFalse("Node still exists and occupied", getNode(DELETED_NODE).toComputer().isIdle());
+        assertFalse("Node still exists and occupied", j.getNode(DELETED_NODE).toComputer().isIdle());
 
         task.done.signal();
         j.waitUntilNoActivity();
@@ -195,7 +201,7 @@ public class PoolTest {
         Pool pool = Pool.getInstance();
         Pool.Updater updater = Pool.Updater.getInstance();
 
-        GitClient cr = injectDummyConfigRepo();
+        GitClient cr = j.injectDummyConfigRepo();
         cr.getWorkTree().child("config").write("No orchestrator url here", "cp1250" /*muahaha*/);
         cr.add("*");
         cr.commit("Break it!");
@@ -203,7 +209,7 @@ public class PoolTest {
         assertThat(pool.getError().getMessage(), startsWith("No orchestrator.url specified by Config Repository"));
         assertTrue(pool.isActivated());
 
-        cr = injectDummyConfigRepo();
+        cr = j.injectDummyConfigRepo();
         cr.getWorkTree().child("config").delete();
         cr.add("*");
         cr.commit("Break it!");
@@ -216,7 +222,7 @@ public class PoolTest {
 
     @Test @Ignore
     public void ui() throws Exception {
-        injectDummyConfigRepo();
+        j.injectDummyConfigRepo();
         Timer.get().schedule(new Runnable() {
             private final Random rand = new Random();
             @Override public void run() {
@@ -240,69 +246,5 @@ public class PoolTest {
             }
         }, 0, TimeUnit.SECONDS);
         j.interactiveBreak();
-    }
-
-    private static class MockTask extends ReservationTask {
-        final SharedComputer actuallyRunOn[] = new SharedComputer[1];
-        public MockTask(@Nonnull ExecutorJenkins owner, @Nonnull Label label) {
-            super(owner, label);
-        }
-
-        @Override
-        public @CheckForNull Queue.Executable createExecutable() throws IOException {
-            return new ReservationExecutable(this) {
-                @Override
-                public void run() throws AsynchronousExecution {
-                    actuallyRunOn[0] = (SharedComputer) Executor.currentExecutor().getOwner();
-                    perform();
-                }
-            };
-        }
-
-        public void perform() {
-            // NOOOP until overriden
-        }
-    }
-
-    private @Nonnull SharedNode getNode(String name) {
-        Node node = j.jenkins.getNode(name);
-        assertNotNull("No such node " + name, node);
-        return (SharedNode) node;
-    }
-
-    private GitClient injectDummyConfigRepo() throws Exception {
-        File orig = new File(getClass().getResource("dummy_config_repo").toURI());
-        assertTrue(orig.isDirectory());
-        File repo = tmp.newFolder();
-        FileUtils.copyDirectory(orig, repo);
-
-        StreamTaskListener listener = new StreamTaskListener(System.err, Charset.defaultCharset());
-        GitClient git = Git.with(listener, new EnvVars()).in(repo).using("git").getClient();
-        git.init();
-        git.add("*");
-        git.commit("Init");
-
-        System.setProperty(CONFIG_REPO_PROPERTY_NAME, repo.getAbsolutePath());
-        Pool.Updater.getInstance().doRun();
-
-        return git;
-    }
-
-    private static class BlockingTask extends MockTask {
-        final OneShotEvent running = new OneShotEvent();
-        final OneShotEvent done = new OneShotEvent();
-
-        public BlockingTask(Label label) {
-            super(PoolTest.DUMMY_OWNER, label);
-        }
-
-        @Override public void perform() {
-            running.signal();
-            try {
-                done.block();
-            } catch (InterruptedException e) {
-                // Proceed
-            }
-        }
     }
 }
