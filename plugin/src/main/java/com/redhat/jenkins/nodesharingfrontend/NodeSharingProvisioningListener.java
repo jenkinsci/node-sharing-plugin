2ba62d0b4288f9425726820bce76772b1f7411cf
Oliver Gondža <ogondza@gmail.com>
2018-01-16 13:07:31 +0100

    Silent useless logs

--- plugin/src/main/java/com/redhat/jenkins/nodesharingfrontend/NodeSharingProvisioningListener.java
+++ plugin/src/main/java/com/redhat/jenkins/nodesharingfrontend/NodeSharingProvisioningListener.java
@@ -15,13 +15,14 @@ import java.util.logging.Logger;
 /**
  * Created by pjanouse on 5/2/17.
  */
+//TODO Do we need this?
 @Extension
 public class NodeSharingProvisioningListener extends CloudProvisioningListener {
     private static final Logger LOGGER = Logger.getLogger(NodeSharingProvisioningListener.class.getName());
 
     @Override
     public CauseOfBlockage canProvision(Cloud c, Label l, int n) {
-        LOGGER.info("NodeSharingProvisioningListener.canProvision() called for '"
+        LOGGER.fine("NodeSharingProvisioningListener.canProvision() called for '"
                 + l + ", workload: " + n);
         try {
             super.canProvision(c, l, n);
@@ -34,7 +35,7 @@ public class NodeSharingProvisioningListener extends CloudProvisioningListener {
 
     @Override
     public void onStarted(Cloud c, Label l, Collection<NodeProvisioner.PlannedNode> n) {
-        LOGGER.info("NodeSharingProvisioningListener.onStarted() called for '"
+        LOGGER.fine("NodeSharingProvisioningListener.onStarted() called for '"
                 + l + ", size: " + n.size());
         try {
             super.onStarted(c, l, n);
@@ -46,7 +47,7 @@ public class NodeSharingProvisioningListener extends CloudProvisioningListener {
 
     @Override
     public void onComplete(NodeProvisioner.PlannedNode pn, Node n) {
-        LOGGER.info("NodeSharingProvisioningListener.onCompleted() called for '"
+        LOGGER.fine("NodeSharingProvisioningListener.onCompleted() called for '"
                 + pn.displayName + "'; '" + n.getDisplayName() + "'");
         try {
             super.onComplete(pn, n);
@@ -58,7 +59,7 @@ public class NodeSharingProvisioningListener extends CloudProvisioningListener {
 
     @Override
     public void onFailure(NodeProvisioner.PlannedNode pn, Throwable t) {
-        LOGGER.info("NodeSharingProvisioningListener.onFailure() called for '"
+        LOGGER.fine("NodeSharingProvisioningListener.onFailure() called for '"
                 + pn.displayName + "'; '" + t.getMessage() + "'");
         try {
             super.onFailure(pn, t);
